0196b7b798adca8e851e2a4a2ef32896f468d1b0
RicardoBatel <ricardo.batel@hotmail.com>
2022-12-26 17:06:52 -0300

    registrando usuários

--- App/Route.php
+++ App/Route.php
@@ -19,6 +19,12 @@ class Route extends Bootstrap {
             'action' => 'inscreverse'
         );
 
+        $routes['registrar'] = array(
+            'route' => '/registrar',
+            'controller' => 'indexController',
+            'action' => 'registrar'
+        );
+
         $this->setRoutes($routes);
     } 
 }
