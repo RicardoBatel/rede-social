cd3a5cb797337a1d2f05b6256bfaca4ded2c4e68
RicardoBatel <ricardo.batel@hotmail.com>
2022-12-27 10:23:33 -0300

    Autenticação de usuário

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -8,7 +8,9 @@ use MF\Model\Container;
 
 class IndexController extends Action {
 
-    public function index() {        
+    public function index() {
+        
+        $this->view->login = isset($_GET['login']) ? $_GET['login'] : '';
 
         $this->render('index');
     }
