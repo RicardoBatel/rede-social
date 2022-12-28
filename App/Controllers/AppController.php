820f3a5528b5f16d4e5ff46562b112fa21e7d082
RicardoBatel <ricardo.batel@hotmail.com>
2022-12-28 11:46:05 -0300

    seguindo e deixando de seguir outros usuários

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -72,4 +72,26 @@ class AppController extends Action {
         $this->render('quemSeguir');
     }
 
+    public function acao() {
+
+        $this->validaAutenticacao();
+
+		$acao = isset($_GET['acao']) ? $_GET['acao'] : '';
+		$id_usuario_seguindo = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
+
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('id', $_SESSION['id']);
+
+		if($acao == 'seguir') {
+			$usuario->seguirUsuario($id_usuario_seguindo);
+
+		} else if($acao == 'deixar_de_seguir') {
+			$usuario->deixarSeguirUsuario($id_usuario_seguindo);
+
+		}
+
+        header('Location: /quem_seguir');
+
+    }
+
 }
